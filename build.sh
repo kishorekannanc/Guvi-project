a882cbc794b2f35bcf58f79c549fd6c55d45274a
kishorekannanc <167565245+kishorekannanc@users.noreply.github.com>
2024-11-21 23:06:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 docker build -t devops-react-app .
 
 # Tag the image for Docker Hub (use your Docker Hub username)
-docker tag devops-react-app kishorekannan23/dev:dev
+docker tag devops-react-app kishorekannan23/prod:prod
 #docker tag devops-react-app kishorekannan23/prod:prod
 
 # Push the image to Docker Hub
-docker push kishorekannan23/dev:dev
+docker push kishorekannan23/prod:prod
 
